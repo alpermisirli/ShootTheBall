0ddb4f5faf0837cb12ae268b7c6735b17416fd1d
RedLegend97 <alpermisirli@gmail.com>
2021-08-23 18:27:03 +0300

    Ball slingshot

--- Shoot The Ball Project/Assets/Scripts/BallHandler.cs
+++ Shoot The Ball Project/Assets/Scripts/BallHandler.cs
@@ -7,8 +7,11 @@ using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D currentBallRigidbody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private float detachDelay;
     private Camera mainCamera;
     private bool isDragging;
+
     private void Start()
     {
         mainCamera = Camera.main;
@@ -17,12 +20,23 @@ public class BallHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        if (currentBallRigidbody == null)
         {
-            currentBallRigidbody.isKinematic = false; //Physics enabled on the object
             return;
         }
 
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            if (isDragging)
+            {
+                LaunchBall();
+            }
+
+            isDragging = false;
+            return;
+        }
+
+        isDragging = true;
         currentBallRigidbody.isKinematic = true; //Physics disabled on the object
 
         Vector2 touchPos = Touchscreen.current.primaryTouch.position.ReadValue(); //Screen pixel wise location of touch
@@ -32,5 +46,18 @@ public class BallHandler : MonoBehaviour
         // Debug.Log("Touch pos:" + touchPos);
         // Debug.Log("World pos:" + worldPos);
         currentBallRigidbody.position = worldPos;
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false; //Physics enabled on the object
+        currentBallRigidbody = null;
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
     }
 }
